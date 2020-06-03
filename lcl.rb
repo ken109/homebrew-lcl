4acf955986953693924807e6825bc3c24a11aeac
kensuke kubo <kensukekubo19@gmail.com>
2020-06-03 15:52:47 +0900

    formura 0.1.4

--- lcl.rb
+++ lcl.rb
@@ -1,8 +1,8 @@
 class Lcl < Formula
   desc ""
   homepage ""
-  url "https://github.com/ken109/homebrew-lcl/archive/0.1.3.tar.gz"
-  sha256 "58b1a8124cda368b78cc75eaa48bc3e7442e160e7a3b09a4e3bbb24a77358463"
+  url "https://github.com/ken109/homebrew-lcl/archive/0.1.4.tar.gz"
+  sha256 "2a3fcefbbb071d0c2108def574bb7a4ffe66c3f2304835672f0e5f5819e66705"
 
   def install
       bin.install "bin/lcl"
