c233dc0d70373f16f9676c81e670832ce1f407ec
kensuke kubo <kensukekubo19@gmail.com>
2020-06-27 02:14:08 +0900

    formura 1.0.3

--- lcl.rb
+++ lcl.rb
@@ -1,13 +1,13 @@
 class Lcl < Formula
   desc "Build local environment"
   homepage "https://github.com/ken109/lcl"
-  url "https://github.com/ken109/lcl/archive/1.0.2.tar.gz"
-  sha256 "dbd5429579193717307d3031af3bd996b5b088de70ce98c210107efda6b3565c"
+  url "https://github.com/ken109/lcl/archive/1.0.3.tar.gz"
+  sha256 "ffbde775e01ca5769ec583ac69a0de0faeb53076c672946fe95dfbcb69597be0"
 
   depends_on "go" => :build
 
   def install
-    etc.install Dir["etc"] => "lcl"
+    etc.install Dir["etc/lcl"]
 
     ENV["GOPATH"] = buildpath
     lcl_path = buildpath/"src/github.com/ken109/lcl/"
