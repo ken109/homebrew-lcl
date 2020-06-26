5c0769c5134189cc404c8d4b09742c3c19d1eb4a
kensuke kubo <kensukekubo19@gmail.com>
2020-06-27 01:29:40 +0900

    formura 1.0.0

--- lcl.rb
+++ lcl.rb
@@ -4,15 +4,13 @@ class Lcl < Formula
   url "https://github.com/ken109/lcl/archive/1.0.0.tar.gz"
   sha256 "72d698aff352e8f97bc86e426cba5e1844acfa578b490d28044f3cd6983bdde8"
 
-  depends_on "dep" => :build
-
   def install
     ENV["GOPATH"] = buildpath
     lcl_path = buildpath/"src/github.com/ken109/lcl/"
     lcl_path.install buildpath.children
 
     cd lcl_path do
-      system "dep", "ensure", "-vendor-only"
+      system "go", "mod", "download"
       system "go", "build"
       bin.install "lcl"
     end
