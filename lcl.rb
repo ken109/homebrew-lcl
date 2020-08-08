874707abca6497760ab84513866423638a3c096a
Kensuke Kubo <kensukekubo19@gmail.com>
2020-08-08 15:40:34 +0000

    Formura 1.2.0

--- lcl.rb
+++ lcl.rb
@@ -1,8 +1,8 @@
 class Lcl < Formula
   desc "Build local environment"
   homepage "https://github.com/ken109/lcl"
-  url "https://github.com/ken109/lcl/archive/1.1.10.tar.gz"
-  sha256 "aa72092d42a061059c00f430ed0e3359b509ddb359cb9367211e1fee8b8df9c2"
+  url "https://github.com/ken109/lcl/archive/1.2.0.tar.gz"
+  sha256 "b95f403bf677dad712764e02235795bd7f37ef131923abde895b06f6a477dd8b"
 
   depends_on "go" => :build
 
