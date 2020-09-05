e19a150b72ace498b3928b478aefc22f6ddde7d7
Kensuke Kubo <kensukekubo19@gmail.com>
2020-09-05 06:14:14 +0000

    Formura 1.0.3

--- srv.rb
+++ srv.rb
@@ -1,8 +1,8 @@
 class Srv < Formula
   desc "Build staging environment"
   homepage "https://github.com/ken109/srv"
-  url "https://github.com/ken109/srv/archive/1.0.2.tar.gz"
-  sha256 "7fa5373a8bb23d5a1f5f8282f5165db446176f4272d22b620180e2033c90a9bd"
+  url "https://github.com/ken109/srv/archive/1.0.3.tar.gz"
+  sha256 "0e15c8f6aaa8fe38b49d56f0c3f6e5ca48ba609666cd085df9ed8d7c7a711667"
 
   depends_on "go" => :build
 
